1a24b62f3e073630ca155a9d030ea1b1b5f82e2a
Roman Shamin <al4emist.artway@gmail.com>
2015-03-03 10:06:43 +0300

    Play with function composition

--- drafts.elm
+++ drafts.elm
@@ -1,9 +1,13 @@
 import Text (asText)
-import Graphics.Collage (..)
-import List (map, (::), head, tail)
 
-data = [ { size={ w=91, h=92 }, id=1 }
-       , { size={ w=93, h=94 }, id=2 }
-       , { size={ w=95, h=96 }, id=3 } ]
+data = 4
 
-main = map (\item -> (item.size.w, item.size.h)) data |> asText
+x2 x = 2 * x
+x3 x = 3 * x
+
+minus2 x = x - 2
+
+pipe1 = minus2 >> x2 >> x3
+pipe2 = x2 >> minus2 >> x3
+
+main = data |> pipe1 |> asText
