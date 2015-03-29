3847b62dd5242e54a9642bd3ad242b93d8abf218
Roman Shamin <al4emist.artway@gmail.com>
2015-03-29 16:39:23 +0300

    Play a bit with time

--- drafts.elm
+++ drafts.elm
@@ -1,35 +1,5 @@
-import Signal (Signal, (<~), (~), map, keepWhen, sampleOn, foldp, merge)
-import List ((::))
-import Mouse
+import Signal (Signal, (<~), (~), map)
 import Text (asText)
 import Time (fps)
 
-type Update = MouseClicks (Int,Int) | MouseDrags (Int,Int)
-
-state =
-  { figures = []
-  , currentPos = { x = 0, y = 0 }
-  }
-
-inputs : Signal Update
-inputs =
-  merge
-    (map MouseClicks (sampleOn Mouse.isDown Mouse.position))
-    (map MouseDrags (keepWhen Mouse.isDown (0,0) Mouse.position))
-
-update input state =
-  case input of
-    MouseClicks coords ->
-      { state | figures <- coords :: state.figures }
-
-    MouseDrags (x,y) ->
-      let updateCurrentPos pos x' y' =
-            { pos | x <- x', y <- y' }
-      in
-          { state | currentPos <- updateCurrentPos state.currentPos x y }
-
-
-currentState =
-  foldp update state inputs
-
-main = asText <~ currentState
+main = asText <~ map (\t -> t / 20) (fps 15)
